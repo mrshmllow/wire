bd77046e60dc7592fb03bfdfc4cad06f4dc71260
marshmallow <marshycity@gmail.com>
2024-07-12 15:49:45 +1000

    init
    Change-Id: Ic11740ed02dab97711da947684f420defe4923cc

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    devenv.url = "github:cachix/devenv";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs = {nixpkgs.follows = "nixpkgs";};
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    ...
+  } @ inputs: let
+    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux"];
+  in {
+    packages = forAllSystems (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+    });
+
+    devShells = forAllSystems (system: {
+      default = devenv.lib.mkShell {
+        inherit inputs;
+        pkgs = nixpkgs.legacyPackages.${system};
+        modules = [
+          {
+            languages.rust.enable = true;
+            languages.rust.channel = "stable";
+
+            pre-commit.hooks = {
+              clippy.enable = true;
+              cargo-check.enable = true;
+              alejandra.enable = true;
+              statix.enable = true;
+              deadnix.enable = true;
+            };
+          }
+        ];
+      };
+    });
+  };
+}
