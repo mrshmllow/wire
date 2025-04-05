e90fc6d450eafedd60c78c51f6fb929704ae7cc6
marshmallow <marshycity@gmail.com>
2025-04-05 12:04:14 +1100

    add cache.nix to checkPhase

--- doc/snippets/default.nix
+++ doc/snippets/default.nix
@@ -3,4 +3,5 @@
   getting-started-hm-flake = import ./getting-started/hm.flake.nix;
   getting-started-nixos = import ./getting-started/configuration.nix;
   getting-started-nixos-flake = import ./getting-started/nixos.flake.nix;
+  getting-started-cache = import ./getting-started/cache.nix;
 }
