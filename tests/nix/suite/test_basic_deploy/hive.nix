7265512b68473866dc3d307dcae1d4edb7dfeedb
itslychee <itslychee@protonmail.com>
2025-04-12 09:59:11 -0500

    testing: clean up hive.nix and return a functor for utils.nix

--- tests/nix/suite/test_basic_deploy/hive.nix
+++ tests/nix/suite/test_basic_deploy/hive.nix
@@ -1,9 +1,9 @@
 let
-  utils = import ../utils.nix { testName = "test_basic_deploy"; };
+  mkHiveNode = import ../utils.nix { testName = "test_basic_deploy"; };
 in
 {
-  meta.nixpkgs = <nixpkgs>;
-  receiver = utils.mkHiveNode "x86_64-linux" "receiver" {
+  meta.nixpkgs = import <nixpkgs> { system = "x86_64-linux"; };
+  receiver = mkHiveNode { hostname = "receiver"; } {
     environment.etc."a".text = "b";
   };
 }
