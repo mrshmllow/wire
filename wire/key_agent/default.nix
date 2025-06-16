f2018c2a38d4442b212619f8e335743ee2d3420c
marshmallow <marshycity@gmail.com>
2025-06-16 10:10:50 +1000

    give key_agent a more descriptive name

--- wire/key_agent/default.nix
+++ wire/key_agent/default.nix
@@ -2,13 +2,14 @@
   perSystem =
     {
       buildRustProgram,
+      system,
       ...
     }:
     {
       packages = {
         agent = buildRustProgram {
           name = "key_agent";
-          pname = "key_agent";
+          pname = "wire-tool-key_agent-${system}";
           cargoExtraArgs = "-p key_agent";
         };
       };
