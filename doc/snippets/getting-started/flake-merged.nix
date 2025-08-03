3f69ba499f7d833d73b27f0b28c0f9182e251b8f
marshmallow <github@althaea.zone>
2025-08-04 09:27:59 +1000

    bring flake docs up to date with makeHive

--- doc/snippets/getting-started/flake-merged.nix
+++ doc/snippets/getting-started/flake-merged.nix
@@ -1,23 +1,38 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.wire.url = "github:wires-org/wire";
 
-  outputs = {nixpkgs, ...} @ inputs: {
-    colmena = {
-      node-a = {
-        deployment = {
-          target = {
-            # ...
-          };
+  outputs = {
+    self,
+    nixpkgs,
+    wire,
+    ...
+  } @ inputs: {
+    wire = wire.makeHive {
+      # Give wire our ninixosConfigurations
+      inherit (self) nixosConfigurations;
 
-          keys = {
-            # ...
-          };
-        };
+      meta = {
+        # ... from above
+      };
+
+      node-a.deployment = {
+        # ...
       };
     };
 
     nixosConfigurations = {
       node-a = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs;};
+        modules = [
+          {
+            nixpkgs.hostPlatform = "x86_64-linux";
+          }
+        ];
+      };
+
+      node-b = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = {inherit inputs;};
         modules = [
--- doc/snippets/getting-started/flake.nix
+++ doc/snippets/getting-started/flake.nix
@@ -1,8 +1,22 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.wire.url = "github:wires-org/wire";
 
-  outputs = _: {
-    colmena = {
+  outputs = inputs @ {
+    nixpkgs,
+    wire,
+    ...
+  }: {
+    wire = wire.makeHive {
+      meta = {
+        nixpkgs = import nixpkgs {
+          system = "x86_64-linux";
+        };
+        specialArgs = {
+          inherit inputs;
+        };
+      };
+
       defaults = {
         # ...
       };
