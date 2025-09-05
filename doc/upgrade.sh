9735738525a5b9db32a8ba7285a4ac8bbe1185ce
marshmallow <github@althaea.zone>
2025-09-05 11:10:21 +1000

    fix a few sed bugs in upgrade.sh

--- doc/upgrade.sh
+++ doc/upgrade.sh
@@ -4,10 +4,11 @@ set -e -x
 
 pushd doc/
 
-sed -i -r -e 's/sha256-[a-zA-Z0-9+]+=/sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=/g' package.nix
+# https://datatracker.ietf.org/doc/html/rfc4648#section-4
+sed -i -r -e 's|sha256-[a-zA-Z0-9+/]+=|sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=|g' package.nix
 
-NEW="$(nix build .#docs 2>&1 | sed -n -r 's/got:\s+(sha256-[a-zA-Z0-9+]+=)/\1/p' | awk '{$1=$1;print}')"
+NEW="$(nix build .#docs 2>&1 | sed -n -r 's/got:\s+(sha256-[a-zA-Z0-9+\\/]+=)/\1/p' | awk '{$1=$1;print}')"
 
 echo "new: $NEW"
 
-sed -i -r -e "s/sha256-[a-zA-Z0-9+]+=/$NEW/g" package.nix
+sed -i -r -e "s|sha256-[a-zA-Z0-9+/]+=|$NEW|g" package.nix
