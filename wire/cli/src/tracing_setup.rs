f52bbe6b2f4776e1c9f7c0e2d22fc26c9b2e7af4
marshmallow <github@althaea.zone>
2025-10-22 20:56:04 +1100

    impl non-verbose log formatter

--- wire/cli/src/tracing_setup.rs
+++ wire/cli/src/tracing_setup.rs
@@ -9,8 +9,18 @@ use std::{
 
 use clap_verbosity_flag::{Verbosity, WarnLevel};
 use lib::STDIN_CLOBBER_LOCK;
+use tracing::{Level, Subscriber};
 use tracing_log::AsTrace;
-use tracing_subscriber::{Layer, Registry, layer::SubscriberExt, util::SubscriberInitExt};
+use tracing_subscriber::{
+    Layer, Registry,
+    fmt::{
+        FormatEvent, FormatFields,
+        format::{self, Format, Full},
+    },
+    layer::SubscriberExt,
+    registry::LookupSpan,
+    util::SubscriberInitExt,
+};
 
 struct NonClobberingWriter {
     queue: VecDeque<Vec<u8>>,
@@ -62,13 +72,39 @@ impl Write for NonClobberingWriter {
     }
 }
 
+struct WireFormat(Format<Full, ()>);
+
+impl<S, N> FormatEvent<S, N> for WireFormat
+where
+    S: Subscriber + for<'a> LookupSpan<'a>,
+    N: for<'a> FormatFields<'a> + 'static,
+{
+    fn format_event(
+        &self,
+        ctx: &tracing_subscriber::fmt::FmtContext<'_, S, N>,
+        writer: tracing_subscriber::fmt::format::Writer<'_>,
+        event: &tracing::Event<'_>,
+    ) -> std::fmt::Result {
+        let metadata = event.metadata();
+
+        if !matches!(metadata.level(), &Level::INFO) {
+            return self.0.format_event(ctx, writer, event);
+        }
+
+        self.0.format_event(ctx, writer, event)?;
+
+        Ok(())
+    }
+}
+
 pub fn setup_logging(verbosity: Verbosity<WarnLevel>) {
     let filter = verbosity.log_level_filter().as_trace();
     let registry = tracing_subscriber::registry();
 
-    let layer = tracing_subscriber::fmt::layer::<Registry>()
-        .without_time()
-        .with_target(false)
+    let event_formatter = WireFormat(format::format().without_time().with_target(false));
+
+    let layer = tracing_subscriber::fmt::layer()
+        .event_format(event_formatter)
         .with_writer(NonClobberingWriter::new)
         .with_filter(filter);
 
