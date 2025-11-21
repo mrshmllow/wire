ddaff58c7f13d956ad758519b1dcce034a8f8d4c
marshmallow <git@althaea.zone>
2025-11-21 12:37:52 +1100

    remove nonexistant nix file in test eval

--- doc/snippets/default.nix
+++ doc/snippets/default.nix
@@ -1,5 +1,4 @@
 {
   getting-started-nixos = import ./getting-started/configuration.nix;
   getting-started-nixos-flake = import ./getting-started/nixos.flake.nix;
-  getting-started-cache = import ./getting-started/cache.nix;
 }
